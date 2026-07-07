39b3e03c419345733a086d8d52168d20d25ba290
L2N6H5B3 <lbrad@outlook.com>
2026-07-07 09:34:48 +0930

    Clear out old code

--- SoftSledWPF/Components/Utility/Enums.cs
+++ SoftSledWPF/Components/Utility/Enums.cs
@@ -1,8 +1,4 @@
 ﻿namespace SoftSledWPF.Components.Utility {
-    public enum WMCRenderMode {
-        GDI, // GDI Renderer via RDP Console
-        RUI  // Remote UI Renderer via RRSP2
-    }
 
     public enum MediaType {
         Video,
